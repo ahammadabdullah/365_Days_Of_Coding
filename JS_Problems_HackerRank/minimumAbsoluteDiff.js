99f89aefcf56f628f00d70cc7ff6b86347cd34ae
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2024-01-14 22:13:02 +0600

    updated

--- JS_Problems_HackerRank/minimumAbsoluteDiff.js
+++ JS_Problems_HackerRank/minimumAbsoluteDiff.js
@@ -11,4 +11,14 @@ function minimumAbsoluteDifference(arr) {
 }
 
 const arr = [3, -7, 0];
-console.log(minimumAbsoluteDifference(arr));
+// console.log(minimumAbsoluteDifference(arr));
+
+// What Javascript statement in place of "?" will make the result always be between 6 and 7?
+const x = 2;
+let y = 4;
+function update(arg) {
+  return Math.random() + y * arg;
+}
+y = 2;
+const result = update(x) + (3 - Math.random());
+console.log(result);
